57f44672ce21049be27db9b8225208400bc5058f
demonic <ian910297@gmail.com>
2019-08-14 16:50:56 +0800

    Make lesson2 execute successfully but I dont know what happened

--- arm/lesson02/include/utils.h
+++ arm/lesson02/include/utils.h
@@ -5,5 +5,6 @@ extern void delay(unsigned long);
 extern void put32(unsigned long, unsigned int);
 extern unsigned int get32(unsigned long);
 extern int get_el(void);
+extern void memzero(unsigned long src, unsigned long n);
 
 #endif  /*_BOOT_H */
